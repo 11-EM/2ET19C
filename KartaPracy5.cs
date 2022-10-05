9aabfd869db0ff4820c5908acc31d6a6f452f69a
Eryk Małolepszy <114140607+eroX799@users.noreply.github.com>
2022-10-05 12:11:29 +0200

    Wykonana KartaPracy5.pdf

---
+++ KartaPracy5.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KartaPracy5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zad 1
+            /*
+            int suma = 0;
+            int dzielniki;
+            for(int i = 10; i < 100; i++)
+            {
+                dzielniki = 0;
+                for(int j = 2; j < i/2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        dzielniki++;
+                        break;
+                    }
+                    if(dzielniki == 0)
+                    {
+                        suma = suma + i;
+                    }
+                    Console.WriteLine(suma);
+                }
+            }
+            */
+
+        }
+    }
+}
