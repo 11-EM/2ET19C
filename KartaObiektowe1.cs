76f5112b564ad79d7e6ebced814bcf5712e3ff96
Eryk Małolepszy <114140607+11-EM@users.noreply.github.com>
2022-12-16 17:40:56 +0100

    KartaObiektowe1.cs

--- KartaObiektowe1.cs
+++ KartaObiektowe1.cs
@@ -5,59 +5,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Zadania
-{
-    class Material{
-        public string typ;
-        public int id;
-        public double cena;
-        public void setTyp(){
-            System.Console.WriteLine("setTyp");
-        }
-        public void getTyp(){
-            System.Console.WriteLine("getTyp");
-        }
-        public void getCena(){
-            System.Console.WriteLine("getCena");
-        }
-        public void setId(){
-            System.Console.WriteLine("setId");
-        }
-    }
-    class Producent{
-        public string nazwa;
-        public string nip;
-        public void getNazwa(){
-            System.Console.WriteLine("getNazwa");
-        }
-        public void getNip(){
-            System.Console.WriteLine("getNip");
-        }
-        public void setNazwa(){
-            System.Console.WriteLine("setNazwa");
-        }
-    }
-    class Rower{
-        public Producent producent;
-        public string kolor;
-        public Material material;
-        public void jedz(){
-            System.Console.WriteLine("jedz");
-        }
-        public void setColor(){
-            System.Console.WriteLine("setColor");
-        }
-        public void getColor(){
-            System.Console.WriteLine("getColor");
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
+namespace Zadania{
+   
+    class Producent
         {
-            Producent p = new Producent();
-            Material m = new Material();
-            Rower r = new Rower(p, m);
+            private string nazwa;
+            private string nip;
+            public string getNazwa()
+            {
+                return "nazwa";
+            }
+            public string getNip()
+            {
+                return "nip";
+            }
+            public string setNazwa()
+            {
+                return "nazwa";
+            }
+        }
+        class Material
+        {
+            private string typ;
+            private int id;
+            private double cena;
+
+            public string setType()
+            {
+                return "type";
+            }
+            public string getType()
+            {
+                return "type";
+            }
+            public double getCena()
+            {
+                return cena;
+            }
+            public int setId()
+            {
+                return id;
+            }
+        }
+        class Rower
+        {
+            public Producent producent;
+            public Material material;
+            private string Producent;
+            private string Kolor;
+            private string Material;
+            public string jedz()
+            {
+                return "Jade";
+            }
+
+            public void setColor(string aKolor)
+            {
+                this.Kolor = aKolor;
+            }
+            public string getColor()
+            {
+                return Kolor;
+            }
+            public void setMaterial(Material amaterial)
+            {
+                this.material = amaterial;
+            }
+            public Rower(Producent aproducent)
+            {
+                this.producent = aproducent;
+            }
+        }
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Producent p1 = new Producent();
+            Rower r1 = new Rower(p1);
+            Material m1 = new Material();
+            r1.setMaterial(m1);
         }
     }
 }
+
